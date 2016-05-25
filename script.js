f9958380b24643041fcf1e75e2d80f7b8aa1a76c
Jordan Ballard <ballard.jordan@gmail.com>
2016-05-25 09:07:31 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $(".profile").hide();
+    $("#profile").on("click", function(){
+        $(".profile").toggle(150);
+    });
+
+    $(".skills").hide();
+    $("#skills").on("click", function(){
+        $(".skills").toggle(150);
+    });
+
+    $(".work").hide();
+    $("#work").on("click", function(){
+        $(".work").toggle(150);
+    });
+
+    $(".contact").hide();
+    $("#contact").on("click", function(){
+        $(".contact").toggle(150);
+    });
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 300);
+        return false;
+      }
+    }
+  });
+});
